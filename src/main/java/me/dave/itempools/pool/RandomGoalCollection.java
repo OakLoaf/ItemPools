eb184a1c3dec9c85b45db2b91e880d110e0651c6
BuildTools <cooldaveb@live.co.uk>
2024-03-27 14:46:03 +0000

    Added nextGoals method

--- src/main/java/me/dave/itempools/pool/RandomGoalCollection.java
+++ src/main/java/me/dave/itempools/pool/RandomGoalCollection.java
@@ -2,7 +2,10 @@ package me.dave.itempools.pool;
 
 import me.dave.platyutils.utils.RandomCollection;
 import org.bukkit.Material;
+import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class RandomGoalCollection extends GoalCollection {
@@ -11,6 +14,15 @@ public class RandomGoalCollection extends GoalCollection {
     public RandomGoalCollection(ConcurrentHashMap<Material, WeightedGoal> goals) {
         randomCollection = new RandomCollection<>();
         goals.values().forEach(weightedGoal -> randomCollection.add(weightedGoal.getMaterial(), weightedGoal.getWeight()));
+    }
+
+    @NotNull
+    public List<Goal> nextGoals(int amount) {
+        List<Goal> goals = new ArrayList<>();
+        for (int i = 0; i < amount; i ++) {
+            goals.add(nextGoal());
+        }
+        return goals;
     }
 
     public Goal nextGoal() {
