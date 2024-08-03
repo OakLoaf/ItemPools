8aef42923e041f47f764c5207de6d8838294b9fc
OakLoaf <oak@beaconstudios.org>
2024-08-03 18:55:52 +0100

    Updated to Spigot 1.21

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,7 +19,7 @@ repositories {
 }
 
 dependencies {
-    compileOnly("org.spigotmc:spigot:1.20.1-R0.1-SNAPSHOT")
+    compileOnly("org.spigotmc:spigot:1.21-R0.1-SNAPSHOT")
     compileOnly("de.oliver:FancyHolograms:2.3.0")
     compileOnly("me.clip:placeholderapi:2.11.2")
     implementation("org.lushplugins:LushLib:0.7.7")
